385ef026b36b33271149236c38cd437ca90526bd
Vadym Pylypchenko <vadym_pylyp@ukr.net>
2016-10-27 00:06:22 +0300

    1

---
+++ src/test/First.java
@@ -0,0 +1,9 @@
+package test;
+
+/**
+ * Created by Вадим on 04.10.2016.
+ */
+public class First {
+    public static void main(String [] args) {
+        System.out.println("Hello world!");    }
+}
