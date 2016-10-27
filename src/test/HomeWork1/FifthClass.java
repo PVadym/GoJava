d5edb1ec56e08c49013ce886ceb20f3ae46937a9
Vadym Pylypchenko <vadym_pylyp@ukr.net>
2016-10-27 22:12:25 +0300

    ДЗ№1

---
+++ src/test/HomeWork1/FifthClass.java
@@ -0,0 +1,11 @@
+package test.HomeWork1;
+
+/**
+ * Created by Вадим on 27.10.2016.
+ */
+public class FifthClass {
+    public static void main(String[] args) {
+
+        System.out.println("Мой пятый класс.");
+    }
+}
---
+++ src/test/HomeWork1/FirstClass.java
@@ -0,0 +1,13 @@
+package test.HomeWork1;
+
+
+
+/**
+ * Created by Вадим on 27.10.2016.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+
+        System.out.println("Мой первый класс.");
+    }
+}
